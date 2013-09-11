0fb7c03ef2ceaf7c481893bd8925a6c9f36084ba
Andrea Amerigo Aristodemo Gariboldi <andrea.gariboldi@gmail.com>
2013-09-11 16:01:30 +0200

    multistep capacity provisioning

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var dyno= require('./lib/dyn.js'),
 var _parser= require('./lib/parser'), 
     _finder= require('./lib/finder'),
     _refiner= require('./lib/refiner'),
-    _index= require('./lib/indexer');
-    _deep= require('./lib/deep');
-    _steps= require('./lib/capacity');
+    _index= require('./lib/indexer'),
+    _deep= require('./lib/deep'),
+    _modify= require('./lib/capacity');
 
 const _traverse= function (o, fn)
       {
@@ -403,79 +403,7 @@ module.exports= function (opts,cb)
 
             table.modify= function (read,write)
             {
-                var p= dyn.promise(),
-                    _check= function (cb)
-                    {
-                          dyn.describeTable(table._dynamo.TableName,
-                          function (err,data)
-                          {
-                              if (err)
-                                p.trigger.error(err);
-                              else
-                              if (data.Table.TableStatus=='UPDATING')
-                                setTimeout(_check,5000,cb);
-                              else
-                              {
-                                table._dynamo= data.Table;
-                                cb();
-                              }
-                          });
-                    };
-
-                if (!read||!write)
-                  process.nextTick(function ()
-                  { p.trigger.error(new Error('You should specify read and write ProvisionedThroughput')) });
-                else
-                    dyn.describeTable(table._dynamo.TableName,
-                    function (err,data)
-                    {
-                          if (err)
-                              p.trigger.error(err);
-                          else
-                          {
-                              var current= data.Table.ProvisionedThroughput;
-
-                              if (current.ReadCapacityUnits==read
-                                &&current.WriteCapacityUnits==write) 
-                                p.trigger.success();
-                              else
-                              {
-                                 console.log('This may take a while...'.yellow);
-
-                                 var steps= _steps(current.ReadCapacityUnits,
-                                                   read,
-                                                   current.WriteCapacityUnits,
-                                                   write);
-
-                                 async.forEachSeries(steps,function (step,done)
-                                 {
-                                     var sread= step[0], swrite= step[1];
-
-                                     dyn.updateTable(table._dynamo.TableName,sread,swrite,
-                                     function (err,data)
-                                     {
-                                        if (err)
-                                          done(err);
-                                        else
-                                          setTimeout(_check,5000,function ()
-                                          {
-                                             if (sread==read&&swrite==write)
-                                               console.log(('current capacity: '+sread+' read '+swrite+' write')
-                                               .green);
-                                             else
-                                               console.log(('current capacity: '+sread+' read '+swrite+' write')
-                                               .yellow);
-
-                                             done();
-                                          });
-                                     });
-                                 },
-                                 p.should('success'));
-                              }
-                          }
-                    });
-
-                return p;
+                return _modify(dyn,table._dynamo.TableName,read,write);
             };
 
             table.drop= function ()
